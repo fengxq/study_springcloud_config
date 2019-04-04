1c30a7fe8a4e97e627757b277c8b284a58f0221c
fengxinqiang <fxq230@163.com>
2019-04-04 09:45:14 +0800

    Initial commit

---
+++ src/main/java/fz/fxq/study_springcloud_config/StudySpringcloudConfigApplication.java
@@ -0,0 +1,13 @@
+package fz.fxq.study_springcloud_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringcloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringcloudConfigApplication.class, args);
+    }
+
+}
